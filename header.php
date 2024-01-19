9fbdd906c4deba1429402a8abda14c1dc987a466
Oumou Coulibaly <coumou100@gmail.com>
2024-01-19 08:50:56 +0000

    modification de la base de données

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
         <img src="images/logo.png" alt="" height="120px">
       </div>
       <div class="connexion">
-        <img src="" alt="" height="30px">
+        <img src="images/OOjs_UI_icon_userAvatar.svg.png" alt="" height="30px">
         <a href="">Connexion</a>
       </div>
     </div>
@@ -32,16 +32,17 @@
           <div class="collapse justify-content-around navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
               <li class="C">
-                <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
+                <a class="nav-link active" aria-current="page" href="index.php">Acceuil</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Categories
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  <li><a class="dropdown-item" href="#">Web</a></li>
+                  <li><a class="dropdown-item" href="#">Intelligence Artificielle</a></li>
+                  <li><a class="dropdown-item" href="#">Réseaux sociaux</a></li>
+                  <li><a class="dropdown-item" href="#">Mobil</a></li>
                 </ul>
               </li>
               <li class="nav-item">
