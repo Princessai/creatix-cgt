79fe423f291c78c06cee526b1f1b46b66176cef9
Sandra <tionosandra902@gmail.com>
2024-01-23 10:53:26 +0000

    ma page catégorie

--- categories.php
+++ categories.php
@@ -1,8 +1,84 @@
 <?php  include("header.php")  ?>
+<div class="container">
+      
+<div class="box1">
+ <br> <br>
+<table>  
+<h2><strong>catégorie :</strong> Communication, marketing, publicité</h2>
+        <tr>
+            <td class="td">Contenu </td>
+            <td class="td">Contenu </td>
+        </tr>
+        <tr>
+            <td class="td">Contenu </td>
+            <td class="td">Contenu </td>
+        </tr>
+    </table>
+    <br>
+    <br>
+    <br>
+    <table>  
+        <tr>
+            <td class="td">Contenu </td>
+            <td class="td">Contenu </td>
+        </tr>
+        <tr>
+            <td class="td">Contenu </td>
+            <td class="td">Contenu </td>
+        </tr>
+    </table>
+    <br>
+    <br>
+    <br>
+    <table>  
+        <tr>
+            <td class="td">Contenu </td>
+            <td class="td">Contenu </td>
+        </tr>
+        <tr>
+            <td class="td">Contenu </td>
+            <td class="td">Contenu </td>
+        </tr>
+    </table>
+    <br>
+    <br>
+    <br>
+    <table>  
+        <tr>
+            <td class="td">Contenu </td>
+            <td class="td">Contenu </td>
+        </tr>
+        <tr>
+            <td class="td">Contenu </td>
+            <td class="td">Contenu </td>
+        </tr>
+    </table>
+    </div>
+    <br><br>
+    <br>
+    <div class="box2">
 
-<section class="categories">
-
-</section>
-
-
+    <p><strong>Top catégorie </strong> <p>
+        <br>
+    <h6>cdfe</h6>
+        <br>
+        <h6>cdfe</h6>
+        <br>
+        <h6>cdfe</h6>
+        <br>
+        <h6>cdfe</h6>
+        <br>
+        <h6>cdfe</h6>
+        <br>
+        <h6>cdfe</h6>
+        <br>
+        <h6>cdfe</h6>
+        <br>
+        <h6>cdfe</h6>
+        <br>
+        <h6>cdfe</h6>
+        <br>
+    </div>
+    </div>
+    
 <?php  include("footer.php")  ?>
