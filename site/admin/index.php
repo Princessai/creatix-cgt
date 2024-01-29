e7efdff3a356c4dabaacf9da53df3c76161cd33d
levix <primelevixx@gmail.com>
2024-01-29 14:13:12 +0100

    aaaa

---
+++ site/admin/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/tableau-de-bord.css">
+</head>
+<body>
+    <div class="head">
+     aaa  
+    </div>
+</body>
+</html>
