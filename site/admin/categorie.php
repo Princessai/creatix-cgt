cce704871a545f53662b7555ebc6b5568ad73945
Oumou Coulibaly <coumou100@gmail.com>
2024-02-01 18:40:21 +0000

    modif administrateur

--- site/admin/categorie.php
+++ site/admin/categorie.php
@@ -73,12 +73,14 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
                     <div class="accordion-item">
                         <h2 class="accordion-header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                                ADMINS
+                               <a href="admins.php">ADMINISTRATEURS</a> 
                             </button>
                         </h2>
                         <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
                             <div class="accordion-body">
-
+                                    <ul>
+                                        <li><a href="admins.php">Tous les administrateurs</a></li>
+                                    </ul>
                             </div>
                         </div>
                     </div>
