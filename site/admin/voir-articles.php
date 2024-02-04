c0ad0080bb081543d07f893921a90c9b9b8c5a70
Oumou Coulibaly <coumou100@gmail.com>
2024-02-04 21:26:03 +0000

    modif oumou

--- site/admin/voir-articles.php
+++ site/admin/voir-articles.php
@@ -1,24 +1,24 @@
-<?php 
+<?php
 
 
 
 if (isset($_GET['id'])  && !empty($_GET['id'])) {
-    
-    $conn2 = new PDO('mysql:host=localhost;dbname=creatix', 'root', '');
 
-    $id = strip_tags($_GET['id']);
+  $conn2 = new PDO('mysql:host=localhost;dbname=creatix', 'root', '');
 
-
-    $sql2= 'SELECT * FROM `articles` WHERE `id` = :id; ';
-
-    $rearticle = $conn2->prepare($sql2);
-
-    $rearticle->bindvalue(':id', $id, PDO::PARAM_INT);
-
-    $rearticle->execute();
+  $id = strip_tags($_GET['id']);
 
 
-    $rows = $rearticle->fetch();
+  $sql2 = 'SELECT * FROM `articles` WHERE `id` = :id; ';
+
+  $rearticle = $conn2->prepare($sql2);
+
+  $rearticle->bindvalue(':id', $id, PDO::PARAM_INT);
+
+  $rearticle->execute();
+
+
+  $rows = $rearticle->fetch();
 }
 
 //     if (!$row) {
@@ -30,9 +30,10 @@ if (isset($_GET['id'])  && !empty($_GET['id'])) {
 //     header('Location: dashboard.php');
 // 
 
+
 ?>
 
-<?php require_once(__DIR__.'/dashboard-header.php') ?>
+<?php require_once(__DIR__ . '/dashboard-header.php') ?>
 
 <div class="col-sm-9" id="">
   <div class="cont">
@@ -42,25 +43,44 @@ if (isset($_GET['id'])  && !empty($_GET['id'])) {
           <p style="  font-size: 36px;text-align: start ;">DETAILS DE l'ARTICLE <?= $rows['titre'] ?></p>
         </div>
         <div class="col-sm-6">
-          <p style=" text-align: start ;">tous(...)</p>
         </div>
         <div class="col-sm-6">
-          <button> <a href="dashboard.php">RETOUR</a></button> 
+          <button> <a href="dashboard.php">RETOUR</a></button>
         </div>
         <div class="col-sm-12 mt-5">
           <div class="admin-card">
-                 
-          <p>ID :<?= htmlspecialchars($rows['id']) ?></p> 
-<p>TITRE : <?= htmlspecialchars($rows['titre']) ?></p>
-<p>IMAGE:  <?= htmlspecialchars($rows['image']) ?></p>
-<p>CONTENU : <?= htmlspecialchars($rows['contenu']) ?></p>
-<p>DATE DE PUBLICATION : <?= htmlspecialchars($rows['date_public']) ?></p>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>TITRE</th>
+                  <th>IMAGE</th>
+                  <th>CONTENU</th>
+                  <th>DATE DE PUBLICATION</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><?= htmlspecialchars($rows['id']) ?></td>
+                  <td><?= htmlspecialchars($rows['titre']) ?></td>
+                  <td><?= htmlspecialchars($rows['image']) ?></td>
+                  <td class='voir-contenu'><?= htmlspecialchars($rows['contenu']) ?></td>
+                  <td><?= htmlspecialchars($rows['date_public']) ?></td>
+                </tr>
+              </tbody>
+            </table>
 
-          <br>
-          <p> <a href="edit-articles.php?id=<?= $rows['id'] ?>">MODIFIER</a>
-             <a href="suprimer-article.php?id=<?= $rows['id'] ?>" class="suprimer">SUPRIMER</a>
-        </p>
-          
+            <!-- <p>ID :</p>
+            <p>TITRE : </p>
+            <p>IMAGE: </p>
+            <p>CONTENU : </p>
+            <p>DATE DE PUBLICATION : </p>
+ -->
+            <br>
+            <p> <a href="edit-articles.php?id=<?= $rows['id'] ?>">MODIFIER</a>
+              <a href="suprimer-article.php?id=<?= $rows['id'] ?>" class="suprimer">SUPRIMER</a>
+            </p>
+
           </div>
 
         </div>
@@ -71,4 +91,4 @@ if (isset($_GET['id'])  && !empty($_GET['id'])) {
 
 </div>
 
-<?php require_once(__DIR__.'/dashboard-footer.php') ?>
+<?php require_once(__DIR__ . '/dashboard-footer.php') ?>
