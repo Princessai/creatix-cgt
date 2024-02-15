f6caecb96097f2a7b9614cbb2e5031108d9a85ad
Oumou Coulibaly <coumou100@gmail.com>
2024-02-15 14:28:18 +0000

    modification finak

--- site/categories.php
+++ site/categories.php
@@ -12,7 +12,7 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
   $sql2 = "SELECT * FROM articles WHERE categories_id=" . $_GET['id'];
   $rearticle = $conn->prepare($sql2);
   $rearticle->execute();
-  $exe2 = $rearticle->fetch();
+  $exe2 = $rearticle->fetchALL(PDO::FETCH_ASSOC);
 }
 
 ?>
@@ -23,17 +23,22 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
     <div class="col-sm-12">
       <div class="container text-center">
         <div class="row">
+              <?php foreach ($exe2 as $row) {
+                ?>
             <div class="col-sm-4">
-              <div class="card border-0">
-                <img src=<?= "admin/uploads/" . $exe2['image'] ?> class="card-img-top" alt="image article">
-                <div class="card-body">
-                  <h5 class="card-title"><?= $exe2['titre'] ?></h5>
-                  <p class="card-text"><small><?= $exe2['date_public'] ?></small></p>
-                  <a href="article.php?id=<?= $exe2['id'] ?>" class="btn btn-primary">VOIR PLUS</a>
+              
+                <div class="card border-0">
+                  <img src=<?= "admin/uploads/" . $row['image'] ?> class="card-img-top" alt="image article">
+                  <div class="card-body">
+                    <h5 class="card-title"><?= $row['titre'] ?></h5>
+                    <p class="card-text"><small><?= $row['date_public'] ?></small></p>
+                    <a href="article.php?id=<?= $row['id'] ?>" class="btn btn-primary">VOIR PLUS</a>
+                  </div>
                 </div>
-              </div>
-            </div>
-        </div>
+                </div>
+                <?php 
+          } ?>
+          </div>
       </div>
     </div>
   </div>
