ffea2b7ba17b474944ffcfe8d972ba491e5c8989
Oumou Coulibaly <coumou100@gmail.com>
2024-02-15 11:44:57 +0000

    modification final

--- site/propos.php
+++ site/propos.php
@@ -1,28 +1,32 @@
-<?php  include("header.php")  ?>
-<div class="container  mt-5">
+<?php include("header.php")  ?>
+
+<div class="container h-100  mt-5">
   <div class="row">
     <div class="col-sm-12">
-      
-          <h3 class="text-center">  L’équipe éditoriale</h3> 
-  <p class="text-center">
-    L’équipe éditoriale est constituée de :
-          </p>
-<ul  style="font-family:Montserrat; font-size:15px">
-    <li> <strong>Directeur de la publication </strong> : GNakale jonathan levi </li>
-    <li> <strong>Rédacteur en Chef</strong>   : Coulibaly oumou</li>
-    <li> <strong>Rédacteur</strong>  : Tiono Sandra</li>
-</ul>
-–
-–
-– 
-<p  style="font-family:Montserrat; font-size:15px">
-   Pour toute information complémentaire, pour signaler un problème technique sur le blog, contactez l’équipe du Blog Techno via le formulaire de contact.
 
-Le Blog Techno 2022 | Theme by Spiracle Themes 
-</p>
+      <h3 class="text-center"> L’équipe éditoriale</h3>
+      <p class="text-center">
+        L’équipe éditoriale est constituée de :
+      </p>
+      <ul style="font-family:Montserrat; font-size:15px">
+        <li> <strong>Product Owner </strong> : Gnakale Jonathan</li>
+        <li> <strong>Scrum Master</strong> : Coulibaly Oumou</li>
+        <li> <strong>Dev Team</strong> :
+          <ul>
+            <li>Tiono Sandra</li>
+            <li>Coulibaly Oumou</li>
+            <li>Gnakale Jonathan</li>
+          </ul>
+        </li>
+      </ul>
+      <p style="font-family:Montserrat; font-size:15px">
+        Pour toute information complémentaire, pour signaler un problème technique sur le blog, contactez l’équipe du Blog Techno via le formulaire de contact.
 
-        
-   
+        Le Blog Techno 2022 | Theme by Spiracle Themes
+      </p>
+
+
+
     </div>
   </div>
 </div>
